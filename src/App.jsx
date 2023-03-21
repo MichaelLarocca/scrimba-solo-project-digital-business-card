3c81ff3e03195ea39eceea69c3090af85749e87c
Larocca <Larocca@RIVPRO1.net>
2023-03-21 18:32:20 -0400

    Updated css

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Photo from './components/Photo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="ctn">
-      <h1>Hello Scrimba Solo Project!</h1>
+      <Photo />
     </div>
   )
 }
