470efeb88faf00d5288b807b0c6183f3a4acb862
Larocca <Larocca@RIVPRO1.net>
2023-03-22 18:23:44 -0400

    Added image attribution

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,6 +1,9 @@
 import soloPhoto from '../img/Han-Solo.png'
 export default function Photo() {
     return (
-        <img src={soloPhoto} ></img>
+        <div id="ctn-image">
+            <img src={soloPhoto} ></img>
+            <p id="attribution"><em>Photo credit: Lucasfilm Ltd. (2015) - "Star Wars: The Force Awakens"</em></p>
+        </div>
     );
 }
